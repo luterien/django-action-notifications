a87a9be4ae137290f397a56ed25760acefdd09b8
Uğur Yörük <uguryoruk26@gmail.com>
2017-04-21 05:50:55 -0700

    fix

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-action-notifications',
-    version='0.1',
-    packages=find_packages(),
+    version='0.2',
+    packages=["notifications",],
     include_package_data=True,
     license='BSD License',
     description='Reusable Django app to save user actions & send them notifications.',
@@ -24,6 +24,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
